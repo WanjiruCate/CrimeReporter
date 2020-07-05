2cae06ef4c90a9ed7538a08c4b8d974325e539b0
Wanjiru Catherine <catherinemunene802@gmail.com>
2020-07-05 03:41:04 +0300

    Refractored auth

--- lib/screens/auth.dart
+++ lib/screens/auth.dart
@@ -1,63 +1,96 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'maps.dart';
 
 class Auth extends StatelessWidget {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final GoogleSignIn _googleSignIn = new GoogleSignIn();
 
-  Future<FirebaseUser> _signIn() async {
-    GoogleSignInAccount googleSignInAccount = await _googleSignIn.signIn();
-    GoogleSignInAuthentication googleSignInAuthentication =
-        await googleSignInAccount.authentication;
-
-    final AuthCredential credential = GoogleAuthProvider.getCredential(
-        idToken: googleSignInAuthentication.idToken,
-        accessToken: googleSignInAuthentication.accessToken);
-
-    final AuthResult authResult = await _auth.signInWithCredential(credential);
-    final FirebaseUser user = authResult.user;
-    assert(user.email != null);
-    assert(user.displayName != null);
-    assert(!user.isAnonymous);
-    assert(await user.getIdToken() != null);
-
-    final FirebaseUser curentUser = await _auth.currentUser();
-    assert(user.uid == curentUser.uid);
-    print('Signed In User: ${user.displayName}');
-    return user;
-
-    // FirebaseUser firebaseUser = await _auth.signInWithGoogle(
-    //   idToken: googleSignInAuthentication.idToken,
-    //   accessToken: googleSignInAuthentication.accessToken,
-    // );
-    // print("Username:  ${firebaseUser.displayName}");
-    // return firebaseUser;
-  }
-
   @override
   Widget build(BuildContext context) {
+    Future<FirebaseUser> _signIn() async {
+      GoogleSignInAccount googleSignInAccount = await _googleSignIn.signIn();
+      GoogleSignInAuthentication googleSignInAuthentication =
+          await googleSignInAccount.authentication;
+
+      final AuthCredential credential = GoogleAuthProvider.getCredential(
+          idToken: googleSignInAuthentication.idToken,
+          accessToken: googleSignInAuthentication.accessToken);
+
+      final AuthResult authResult =
+          await _auth.signInWithCredential(credential);
+      final FirebaseUser user = authResult.user;
+      assert(user.email != null);
+      assert(user.displayName != null);
+      assert(!user.isAnonymous);
+      assert(await user.getIdToken() != null);
+
+      final FirebaseUser curentUser = await _auth.currentUser();
+      assert(user.uid == curentUser.uid);
+
+      SharedPreferences sharedPreferences =
+          await SharedPreferences.getInstance();
+
+      sharedPreferences.setString('user', user.displayName);
+
+      if (curentUser != null) {
+        Fluttertoast.showToast(
+          msg: 'SignIn Successful',
+          toastLength: Toast.LENGTH_LONG,
+          backgroundColor: Colors.green,
+          textColor: Colors.white,
+        );
+        Navigator.of(context)
+            .push(MaterialPageRoute(builder: (BuildContext context) => Maps()));
+        print('Signed In User: ${user.displayName}');
+      } else {
+        Fluttertoast.showToast(
+          msg: 'SignIn Failed',
+          toastLength: Toast.LENGTH_LONG,
+          backgroundColor: Colors.red,
+          textColor: Colors.white,
+        );
+      }
+      return user;
+
+      // FirebaseUser firebaseUser = await _auth.signInWithGoogle(
+      //   idToken: googleSignInAuthentication.idToken,
+      //   accessToken: googleSignInAuthentication.accessToken,
+      // );
+      // print("Username:  ${firebaseUser.displayName}");
+      // return firebaseUser;
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Firebase Auth'),
         centerTitle: true,
       ),
-      body: Center(
+      body: Container(
+        color: Colors.white,
+        height: MediaQuery.of(context).size.height,
+        width: double.infinity,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             FlatButton(
-              color: Colors.red,
-              child: Text('Sign In'),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+              //  color: Colors.white,
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/google_logo.png'),
+                backgroundColor: Colors.transparent,
+              ),
               onPressed: () {
                 _signIn();
               },
             ),
-            FlatButton(
-              child: Text('Sign In'),
-              onPressed: () {},
-              color: Colors.green,
-            ),
+            Text('Tap to Sign In'),
           ],
         ),
       ),
